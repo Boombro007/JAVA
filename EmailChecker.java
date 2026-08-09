54696f45d17a5c926f0691b7b19773396f21a8ea
Bhavya0921 <boombro42@gmail.com>
2026-08-09 21:02:21 +0530

    Add files via upload

--- EmailChecker.java
+++ EmailChecker.java
@@ -9,13 +9,27 @@ public class EmailChecker
         email = scan.nextLine();
         
         int at = email.indexOf("@");
+        boolean numberPresent = email.matches(".*\\d.*");
         String name = email.substring(at,at+1);
         int size = email.length();
         String domain = email.substring(at,size);
-        
+
+        System.out.println("\t Errors \t");
         if(at < 8)
         {
             System.out.println("enter 8 characters,number,special symbols");
+        }
+        if(!numberPresent)
+        {   
+            System.out.println("Please Enter a number!!!");
+        }
+        if(domain != "gmail.com" || domain != "yahoo.com" || domain != "icloud.com")
+        {
+            System.out.println("Enter correct domain name!!!!");
+        }
+        else
+        {
+            System.out.println("No errors !!!!");
         }
         scan.close();
     }
